db3500e19475960278ad2dbfa436013eb20a42ef
Jakub Żmuda <jzmuda93@gmail.com>
2018-11-14 00:49:40 +0100

    Utworzenie modeli potrzebnej do applikacji.

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')
+    name = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='quizzes')
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+    text = models.CharField('Question', max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.CharField('Answer', max_length=255)
+    is_correct = models.BooleanField('Correct answer', default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    quizzes = models.ManyToManyField(Quiz, through='TakenQuiz')
+    interests = models.ManyToManyField(Subject, related_name='interested_students')
+
+    def __str__(self):
+        return self.user.username
+
+
+class TakenQuiz(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='taken_quizzes')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='taken_quizzes')
+    score = models.FloatField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class StudentAnswer(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='quiz_answers')
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='+')
