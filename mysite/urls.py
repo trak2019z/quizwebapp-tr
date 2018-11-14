bfa3b3dd7abadb2605caaaca5af54982bca79be2
Jakub Żmuda <jzmuda93@gmail.com>
2018-11-15 00:39:46 +0100

    Zmieniony URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from classroom.views import classroom, students, teachers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('classroom.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
+    path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
 ]
