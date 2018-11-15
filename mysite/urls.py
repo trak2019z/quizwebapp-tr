881ad5fbee5f9aabd94a16f31a7bec7b0533ef8b
Jakub Żmuda <jzmuda93@gmail.com>
2018-11-15 11:57:29 +0100

    Kolejne poprawki pod hosting 'url'

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from classroom.views import classroom, students, teachers
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
     path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
